19b619e0c51df4491ab15ae502b8e88cd59e6a16
Ashley <a.clarkem@gmail.com>
2014-08-11 14:57:47 -0600

    unclickable about button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$ (document).ready(function() {
+  $('button').on({
+    mouseover:function(){
+      $(this).css({
+        left:(Math.random()*200)+"px",
+        top:(Math.random()*200)+"px",
+      });
+    }
+  });
+});
